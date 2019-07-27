5ff3b334c829c4bba0772020c3c2b1f79c0c7226
stanley <stanleygomesdasilva@gmail.com>
2019-07-27 08:42:11 -0300

    tipos primitimos e inferencia

--- exercicios/src/fundamentos/AreaCircunferencia.java
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -1,12 +1,12 @@
 package fundamentos;
 
 public class AreaCircunferencia {
-    public static void main (String[] args) {
-        double raio = 3.8;
+    public double calcular () {
+        double raio = 10;
         double PI = 3.1415;
 
         double area = PI * raio * raio;
 
-        System.out.println("Area eh: " + area);
+        return area;
     }
 }
