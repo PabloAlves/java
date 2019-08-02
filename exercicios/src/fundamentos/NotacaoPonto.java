47a675a23a975e9b2f65afd78db1a52220bc8b5f
stanley <stanleygomesdasilva@gmail.com>
2019-08-02 11:08:34 -0300

    conceitos de string e notacao ponto

---
+++ exercicios/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class NotacaoPonto {
+  public static void main(String[] args) {
+    String s = "bom dia";
+    s = s.toUpperCase();
+
+    System.out.println(s);
+  }
+}
