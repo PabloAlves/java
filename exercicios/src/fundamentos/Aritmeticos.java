7c6cbfafe5ba79d368d571db12a8e3e439ea2670
stanley <stanleygomesdasilva@gmail.com>
2019-08-11 12:50:42 -0300

    tipos numericos

---
+++ exercicios/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Aritmeticos {
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 2;
+
+    System.out.println(2 + 3);
+    System.out.println(a / b);
+    System.out.println();
+  }
+}
