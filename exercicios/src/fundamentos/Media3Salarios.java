b3fb723007f74bec502590daacd96bc73cec7ba3
stanley <stanleygomesdasilva@gmail.com>
2019-08-11 12:50:42 -0300

    tipos numericos

---
+++ exercicios/src/fundamentos/Media3Salarios.java
@@ -0,0 +1,13 @@
+// desafio
+
+// receber 3 salarios via scanner (usando string nextline)
+// calcular media dos ultimos 3 salarios
+// receber casas decimais com virgula ou ponto
+
+package fundamentos;
+
+public class Media3Salarios {
+  public static void main(String[] args) {
+
+  }
+}
